d0aa333fd631c3399f99d3c57bec87d69523c6f0
bitsnaps <bitsnaps@yahoo.fr>
2019-11-22 11:58:05 +0100

    Update layout for Bootstrap4
    Update layout for better Bootstrap4 compatibility.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,9 +5,9 @@
 
 use app\widgets\Alert;
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
+use yii\bootstrap4\Nav;
+use yii\bootstrap4\NavBar;
+use yii\bootstrap4\Breadcrumbs;
 use app\assets\AppAsset;
 
 AppAsset::register($this);
@@ -18,8 +18,8 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
@@ -27,19 +27,30 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 <div class="wrap">
-    <?php
+<?php 
     NavBar::begin([
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'navbar navbar-expand-md navbar-dark fixed-top bg-dark',
         ],
     ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
+echo Nav::widget([
+        'options' => ['class' => 'navbar-nav ml-auto'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
+            /*[ // example from yii2-admin extension
+                'label' => 'Admin',
+                'items' => [
+                     ['label' => 'Permission', 'url' => ['/admin/permission']],
+                     ['label' => 'Route', 'url' => ['/admin/route']],
+                     ['label' => 'Menu', 'url' => ['/admin/menu']],
+                     ['label' => 'Role', 'url' => ['/admin/role']],
+                     ['label' => 'Assignment', 'url' => ['/admin/assignment']],
+                     ['label' => 'User', 'url' => ['/admin/user']],
+                ],
+            ],*/
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
@@ -56,22 +67,26 @@ AppAsset::register($this);
         ],
     ]);
     NavBar::end();
-    ?>
-
+?>  
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
-        <?= $content ?>
+        <main role="main">
+            <?= $content ?>
+        </main>            
     </div>
-</div>
+</div><!-- .wrap -->
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
+<footer class="container">
+    <div class="row">
+    <div class="col-md-6">
+        <p>&copy; My Company <?= date('Y') ?></p>
+    </div>
+    <div class="col-md-6">
+        <p class="float-right"><?= Yii::powered() ?></p>
+    </div>
     </div>
 </footer>
 
